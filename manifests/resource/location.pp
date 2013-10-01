d2ff02777c0716faaf140d5cc250ace5dc8bd963
Matthew Brown <mnbbrown@gmail.com>
2013-10-01 19:59:23 +0200

    Correct template paths

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -43,21 +43,21 @@ define nginx::resource::location(
       if ($upstream == undef){
         fail('Cannot create a proxy location if upstream is not defined')
       }
-      $content = template('nginx/vhost/vhost_location_proxy.erb') 
+      $content = template('vhost/vhost_location_proxy.erb') 
     }
 
     directory: { 
       if ($www_root == undef){
         fail('Cannont create a directory location if www_root is not defined')
       }
-      $content = template('nginx/vhost/vhost_location_directory.erb') 
+      $content = template('vhost/vhost_location_directory.erb') 
     }
 
     uwsgi: { 
       if ($upstream == undef){
         fail('Cannot create a uwsgi location if upstream is not defined')
       }
-      $content = template('nginx/vhost/vhost_location_uwsgi.erb') 
+      $content = template('vhost/vhost_location_uwsgi.erb') 
     }
 
   }
